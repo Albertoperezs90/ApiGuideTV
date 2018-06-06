d620f6b9c2166cae1bf8f1ea5f1ef44c463867e1
Alberto Pérez <albertoperezsimon@gmail.com>
2018-06-07 01:22:50 +0200

    Auth endpoint created

---
+++ ApiGuideTV/Auth.aspx.cs
@@ -0,0 +1,25 @@
+﻿using ApiGuideTV.Utilities.Format;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ApiGuideTV
+{
+    public partial class Auth : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GenerateToken();
+        }
+
+        private void GenerateToken()
+        {
+            string token = Facade.Instance.GenerateToken();
+            Context.Response.ContentType = HTMLHeaders.ContentTypeJson();
+            Context.Response.Write(token);
+        }
+    }
+}
